df3688a9b299a60450e50784a75f721fda156692
db <developerstation05@Programming-station-4-iMac-4.local>
2013-10-30 18:11:48 -0400

    adding Qatar Graphic overlay WIP

---
+++ Rad_IOS_MapTest/QTRQatarMapOverlayView.h
@@ -0,0 +1,15 @@
+//
+//  QTRQatarMapOverlayView.h
+//  Rad_IOS_MapTest
+//
+//  Created by Developer Station 05 on 10/30/13.
+//  Copyright (c) 2013 db. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface QTRQatarMapOverlayView : MKOverlayView
+
+- (instancetype)initWithOverlay:(id<MKOverlay>)overlay overlayImage:(UIImage *)overlayImage;
+
+@end
