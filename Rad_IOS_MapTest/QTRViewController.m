b71619f7aa5ad415ef0d936853ceec3bcb59b756
db <developerstation05@Programming-station-4-iMac-4.local>
2013-10-30 17:55:05 -0400

    prepping to add graphic overlay

--- Rad_IOS_MapTest/QTRViewController.m
+++ Rad_IOS_MapTest/QTRViewController.m
@@ -9,11 +9,15 @@
 #import "QTRViewController.h"
 #import <MapKit/MapKit.h>
 #import <AddressBook/AddressBook.h>
+#import "Qatar.h"
 
-@interface QTRViewController ()
+@interface QTRViewController () <MKMapViewDelegate>
 
 @property (strong, nonatomic) MKMapView *QTRView;
 @property (strong, nonatomic) UIButton *QTRButton;
+@property (strong, nonatomic) MKPolygon *demoPolygon;
+@property (strong, nonatomic) MKPolygonRenderer *polyRender;
+@property (strong, nonatomic) Qatar *qatar;
 
 @end
 
@@ -32,6 +36,9 @@
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(
                                                                 CLLocationCoordinate2DMake(24.2, 45.1), 2500000, 2500000);
     [self.QTRView setRegion:region animated:NO];
+    self.QTRView.delegate = self;
+
+    self.qatar = [[Qatar alloc] initWithRegion];
 
 
 }
@@ -53,13 +60,13 @@
     self.QTRButton.backgroundColor = [UIColor whiteColor];
     [self.QTRButton setTitle:@"Qatar" forState:UIControlStateNormal];
     self.QTRButton.titleLabel.textColor = [UIColor blackColor];
-    [self.QTRButton addTarget:self action:@selector(zoomToQatar) forControlEvents:UIControlEventTouchDown];
+    [self.QTRButton addTarget:self action:@selector(zoomToQatarWithAnnotations) forControlEvents:UIControlEventTouchDown];
     [self.view addSubview:self.QTRButton];
 
 
 }
 
--(void) zoomToQatar {
+-(void) zoomToQatarWithAnnotations {
     NSLog(@"Button clicked");
     MKPointAnnotation *pt1 = [self createAnnotationAtCordinate:CLLocationCoordinate2DMake(26.185, 51.21)];
     MKPointAnnotation *pt2 = [self createAnnotationAtCordinate:CLLocationCoordinate2DMake(25.899, 51.68)];
@@ -74,23 +81,34 @@
     ovrlayCoord [3] = pt4.coordinate;
     ovrlayCoord [4] = pt5.coordinate;
     ovrlayCoord [5] = pt6.coordinate;
-    MKPolygon *demoPolygon = [MKPolygon polygonWithCoordinates:ovrlayCoord count:6];
-    [self.QTRView addOverlay:demoPolygon level:MKOverlayLevelAboveRoads];//try using AboveLabels
-                                                                         
+    self.demoPolygon = [MKPolygon polygonWithCoordinates:ovrlayCoord count:6];
+    [self.QTRView addOverlay:self.demoPolygon level:MKOverlayLevelAboveRoads];
+        //[self fadeTheAnnotationsAndOverlay];
 
     [self.QTRView showAnnotations:@[pt1,pt2,pt3,pt4,pt5,pt6] animated:YES];
+
+        //[self fadeTheAnnotationsAndOverlay];
 
 }
 
 -(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay{
         //UIColor *purpleColor = [UIColor colorWithRed:0.149f green:0.0f blue:0.40f alpha:1.0f];
-    UIColor *halfPurpleColor = [UIColor colorWithRed:0.149f green:0.0f blue:0.40f alpha:0.5f];
-    UIColor *halfBlackColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5f];
-        //if(overlay ==stadiumsPolygon){
-        MKPolygonRenderer *polygonRenderer = [[MKPolygonRenderer alloc] initWithOverlay:overlay];
-        polygonRenderer.strokeColor =halfBlackColor;
-        polygonRenderer.fillColor =halfPurpleColor;
-        return polygonRenderer;
+    UIColor *fillColor = [UIColor colorWithHue:.5 saturation:.5 brightness:.5 alpha:.5];
+    UIColor *strokeColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5f];
+        self.polyRender = [[MKPolygonRenderer alloc] initWithOverlay:overlay];
+        self.polyRender.strokeColor =strokeColor;
+        self.polyRender.fillColor =fillColor;
+        return self.polyRender;
+}
+
+-(void)fadeTheAnnotationsAndOverlay {
+
+        //[UIView animateWithDuration:0.6 animations:^{[self.QTRView removeOverlay:self.demoPolygon];} completion:^(BOOL finished) {}];
+    [UIView animateWithDuration:2.0 delay:10 options:UIViewAnimationOptionAllowAnimatedContent animations:^{
+        self.polyRender.fillColor = [UIColor colorWithHue:.5 saturation:.5 brightness:.5 alpha:.5];
+        self.polyRender.strokeColor =[UIColor colorWithRed:0 green:0 blue:0 alpha:0.5f];} completion:^(BOOL finished) {
+        ;}];
+
 }
 
 
